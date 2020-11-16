500ad2e76a5a28332999968685665704cd7df5bf
Kasparas Dijokas <44749080+Kasparas17@users.noreply.github.com>
2020-11-16 11:03:14 +0200

    filter component

--- src/Pages/Menu/Menu.js
+++ src/Pages/Menu/Menu.js
@@ -1,43 +1,85 @@
-import React, { useState } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import classes from "./Menu.module.css";
 import { data } from "../../data";
 import Dish from "../../components/Dish/Dish";
 import divideImg from "../../assets/images/patterns/pattern-divide.svg";
 import Button from "../../components/Button/Button";
+import Filter from "../../components/Filter/Filter";
+import Footer from "../../components/Footer/Footer";
 
 const Menu = () => {
+  const [category, setCategory] = useState("");
+  const [sort, setSort] = useState("");
+  const [length, setLength] = useState("");
+  const [menuData, setmenuData] = useState(data);
+  
+  useEffect(() => {
+    const filtered = category === '' ? data : data.filter((prop) => prop.category === category);
+    setmenuData(filtered);
+  }, [category]);
+
+  //  useEffect(() => {
+  //   let filter = [...menuData]
+  //   if (sort === 'lowest') {
+  //     filter.sort((a, b) => a.price - b.price )
+  //   } else if (sort === 'highest') {
+  //     filter.sort((a, b) => b.price - a.price)
+  //   } else {
+  //     filter = menuData;
+  //   } 
+  //   setmenuData(filter)
+  //  }, [sort]) 
+
+     const sortHandler = (e) => {
+       console.log(e.target.value);
+       setSort(e.target.value)
+    let filter = [...menuData]
+    if (e.target.value === 'lowest') {
+      filter.sort((a, b) => a.price - b.price )
+    } else if (e.target.value === 'highest') {
+      filter.sort((a, b) => b.price - a.price)
+    } else {
+      filter = menuData;
+    } 
+    setmenuData(filter)
+   }
+
   return (
-    <section className={classes.menu_container}>
-      <div className={classes.menu_items}>
-        <ul className={classes.menu_dishes}>
-          {Object.keys(data).map((category) => {
-            return Object.keys(data[category]).map((item, i) => {
+    <>
+      <Filter
+        length={length}
+        category={category}
+        sort={sort}
+        setCategory={setCategory}
+        setSort={setSort}
+        length={menuData.length}
+        sortHandler={sortHandler}
+      />
+      <section className={classes.menu_container}>
+        <div className={classes.menu_items}>
+          <ul className={classes.menu_dishes}>
+            {menuData.map((dish) => {
               return (
-                <li
-                  className={classes.dish_item}
-                  key={data[category][item].alt}
-                >
-                  <img
-                    src={data[category][item].img}
-                    alt={data[category][item].alt}
-                  ></img>
+                <li className={classes.dish_item} key={dish.alt}>
+                  <img src={dish.img} alt={dish.alt}></img>
                   <div>
-                    <h2>{data[category][item].heading}</h2>
-                    <p>{data[category][item].text}</p>
+                    <h3>{dish.heading}</h3>
+                    <p>{dish.text}</p>
                     <div className={classes.price}>
-                      <p>{data[category][item].price} $</p>
+                      <p>{dish.price} $</p>
                       <Button class_name="btn_dark">Add to cart</Button>
                     </div>
                   </div>
                 </li>
               );
-            });
-          })}
-        </ul>
-      </div>
+            })}
+          </ul>
+        </div>
 
-      <div className={classes.menu_cart}></div>
-    </section>
+        <div className={classes.menu_cart}></div>
+      </section>
+      <Footer />
+    </>
   );
 };
 
@@ -52,3 +94,55 @@ export default Menu;
           </p>
           <Button class_name="btn_dark">ORDER NOW</Button> */
 }
+
+{
+  /* {Object.keys(data).map((group) => {
+                return Object.keys(data[group]).map((item, i) => {
+                    return (
+                      <li
+                        className={classes.dish_item}
+                        key={data[group][item].alt}
+                      >
+                        <img
+                          src={data[group][item].img}
+                          alt={data[group][item].alt}
+                        ></img>
+                        <div>
+                          <h3>{data[group][item].heading}</h3>
+                          <p>{data[group][item].text}</p>
+                          <div className={classes.price}>
+                            <p>{data[group][item].price} $</p>
+                            <Button class_name="btn_dark">Add to cart</Button>
+                          </div>
+                        </div>
+                      </li>
+                    );
+                });
+              
+            })} */
+}
+
+//   {Object.keys(menuData).map((group) => {
+//     return Object.keys(menuData[group]).map((item, i) => {
+//         return (
+//           <li
+//             className={classes.dish_item}
+//             key={menuData[group][item].alt}
+//           >
+//             <img
+//               src={menuData[group][item].img}
+//               alt={menuData[group][item].alt}
+//             ></img>
+//             <div>
+//               <h3>{menuData[group][item].heading}</h3>
+//               <p>{menuData[group][item].text}</p>
+//               <div className={classes.price}>
+//                 <p>{menuData[group][item].price} $</p>
+//                 <Button class_name="btn_dark">Add to cart</Button>
+//               </div>
+//             </div>
+//           </li>
+//         );
+//     });
+
+// })}
